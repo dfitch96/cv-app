d4e3c805e2aadeb396005b069b9fec1c36a9dfe7
dfitch96 <kkarl30@gmail.com>
2025-03-31 14:52:28 -0400

    added link to github repo to the main header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
-
+import GitHubIcon from "../assets/github-logo.png";
 
 export default function Header(){
   return (
     <h1 className="header">
+      <a href="https://github.com/dfitch96/cv-app" target="_blank">
+        <img src={GitHubIcon} className="header-icon"></img>
+      </a>
       <div>CV Builder</div>
     </h1>
   );
